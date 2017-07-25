e71639be3821ec6f7ed0f81b85d291f5cd43cba7
MauVlad <nitrox-tk@hotmail.com>
2017-07-25 16:35:33 -0500

    grafica con N n = 10

--- ev3/gra/gd.py
+++ ev3/gra/gd.py
@@ -5,8 +5,8 @@ import numpy as np
 
 ax = pl.gca()
 n = 10 ##1024
-x = 1 ##np.random.normal() ##(0,1,n)
-y = 1 ##np.random.normal() ##(0,1,n)
+x = (1,n) ##np.random.normal() ##(0,1,n)
+y = (1,n) ##np.random.normal() ##(0,1,n)
 
 pl.scatter(x,y)
 pl.savefig('gd.jpg')
