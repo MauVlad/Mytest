166c151fc046a177b845d8284e92cdcab10ff834
MauVlad <nitrox-tk@hotmail.com>
2017-07-25 16:03:33 -0500

    programa grafica se genera una grafica de dispercion

---
+++ ev3/gra/gd.py
@@ -0,0 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pl
+import numpy as np
+
+ax = pl.gca()
+n = 1024
+x = np.random.normal(0,1,n)
+y = np.random.normal(0,1,n)
+
+pl.scatter(x,y)
+pl.savefig('gd.jpg')
