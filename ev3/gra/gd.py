91bf1df864a56e63fe8da57706c58f72c00bff2f
MauVlad <nitrox-tk@hotmail.com>
2017-07-25 16:17:21 -0500

    modifica

--- ev3/gra/gd.py
+++ ev3/gra/gd.py
@@ -5,8 +5,8 @@ import numpy as np
 
 ax = pl.gca()
 n = 1 ##1024
-x = np.random.normal(0,1,n)
-y = np.random.normal(0,1,n)
+x = np.random.normal(0,1)
+y = np.random.normal(0,1)
 
 pl.scatter(x,y)
 pl.savefig('gd.jpg')
