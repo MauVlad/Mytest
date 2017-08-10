9774541856e93f0843fd1f88473c5fd5d62a9065
MauVlad <nitrox-tk@hotmail.com>
2017-08-10 17:31:55 -0500

    c/s
    codigo del cliente y servidor

--- ev3/env.py
+++ ev3/env.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import *
+#from ev3dev.ev3 import *
 from time import sleep
 import zmq
 import random
 
-ir = InfraredSensor()
-ir.mode = 'IR-PROX'
+#ir = InfraredSensor()
+#ir.mode = 'IR-PROX'
 context = zmq.Context()
 s = context.socket(zmq.REQ)
-s.connect("tcp://52.173.88.125:3003")
+s.connect("tcp://192.168.1.79:5376")
 
 while True:
-	d = str (ir.value())
-	print (type(d))
-	s.send_string(d)
+#	d = str (ir.value())
+#	print (type(d))
+	s.send_string('dssdas')
 	m=s.recv()
 	print (m)
 	sleep(1)
--- ev3/server.py
+++ ev3/server.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
-from ev3dev.ev3 import *
 from time import sleep
 import zmq
-import random
 
-ir = InfraredSensor()
-ir.mode = 'IR-PROX'
 context = zmq.Context()
-s = context.socket(zmq.REQ)
-s.connect("tcp://192.168.1.79:3003")
+s = context.socket(zmq.REP)
+s.bind("tcp://192.168.1.79:5376")
 
 while True:
-	d = str (ir.value())
-	print (type(d))
-	s.send_string(d)
+
 	m=s.recv()
 	print (m)
+
+	s.send_string('hpola')
 	sleep(1)
