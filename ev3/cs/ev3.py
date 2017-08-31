30f95e7163662f4e46cc971e0fbcb7e52a4592da
MauVlad <nitrox-tk@hotmail.com>
2017-08-31 18:21:44 -0500

    prueba modificada

--- ev3/cs/ev3.py
+++ ev3/cs/ev3.py
@@ -3,6 +3,7 @@
 ##Librerias##
 from ev3dev.ev3 import *
 from time import sleep
+#import zmq
 
 ##Definiendo los componentes a usar##
 md = MediumMotor ('outA') ## Motor mediano
@@ -11,16 +12,27 @@ mr = LargeMotor ('outC') ## Motor derecho
 
 ir = InfraredSensor(); assert ir.connected, "Por favor, conecte el InfraredSensor"
 ts = TouchSensor(); assert ts.connected, "Por favor, conecte el TouchSensor"
-##cl = ColorSensor(); assert cl.connected, "Por favor, conecte el ColorSensor"
+cl = ColorSensor(); assert cl.connected, "Por favor, conecte el ColorSensor"
 
 ##Modo de uso de los sensores##
 ir.mode = 'IR-PROX'
-#cl.mode = 'COL-AMBIENT'
+cl.mode = 'COL-AMBIENT'
+
+##Crendo y definiendo los puertos de coneccion con el servidor##
+#context = zmq.Context()
+#s = context.socket(zmq.REQ)
+#s1 = context.socket(zmq.REQ)
+#s2 = context.socket(zmq.REQ)
+#s3 = context.socket(zmq.REQ)
+#s.connect("tcp://192.168.1.90:7525")
+#s1.connect("tcp://192.168.1.90:6763")
+#s2.connect("tcp://192.168.1.90:6625")
+#s3.connect("tcp://192.168.1.90:5376")
 
 ##Limite de velocidad de los motores##
 def lim_speed(speed):
-        if speed > 800:
-                speed = 800
+        if speed > 750:
+                speed = 750
         elif speed < 450:
                 speed = 450
         return speed
@@ -38,18 +50,29 @@ def muestra():
 
 def vuelta(a,b,c):
         ml.run_timed(time_sp= a, speed_sp= b, stop_action='brake')
-        print(ml.time_sp,ml.speed_sp)
         mr.run_timed(time_sp= a, speed_sp= c, stop_action='brake')
-        print(mr.time_sp,mr.speed_sp)
         ml.wait_while('runnig')
         mr.wait_while('runnig')
+
+##Definicion de guardado de datos en unarchivo.tx##
+def datos():
+        archi=open('datos/dato.txt', 'a')
+        archi.write('Fecha y Hora: '+ time.strftime("%x ")+ time.strftime("%X")+ '\n')
+        archi.close()
+
+def gdatos(cm,lum,an):
+        archi=open('datos/dato.txt', 'a')
+        archi.write('Distancia avanzada: '+ str(cm) +'cm = '+ str(cm * 0.01) + 'm = '+ str(m * 3.28084) +'ft'+'\n')
+        archi.write('Detecion de luz ambiental: ' + str(lum) +'%'+'\n')
+        archi.write('El angulo de giro fue :'+ str(an) +'\n')
+        archi.close()
 
 ##Datos de las ganancias##
 X_REF = 120
 Y_REF = 150
 KP = 0.4
-KI = 0.01
-KD = 0.005
+KI = 0.02
+KD = 0.05
 GAIN = 10
 
 ##Datos iniciales##
@@ -68,12 +91,17 @@ while not ts.value():
 
         v = ((ir.value()) * 0.7) ##convercion a cm de la distacia detectada
 ###variables para dar vuelta en el mismo eje###
-        z = 0
+        z,an = 0,0
         a,b,c = 0,0,0
 
+#        s.send_string(str(v))
+#        vi=s.recv()
+
 ##Primera condicion por arriba de un valor de v el robot se movera##
-        if v > 35:
-                print (ir.value(), v)
+        if float(vi) > 35:
+
+		lum = cl.value()
+#                s1.send_string(str(lum))
 
                 md.stop()
 ##Funcion del PID para que el motor llegue a su velocidad optima##
@@ -93,11 +121,11 @@ while not ts.value():
                        ml.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
                        mr.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
                 elif ml.position > mr.position:
-                       mr.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
-                       ml.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
+                       mr.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)) + .5)
+                       ml.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)) - .5)
                 else:
-                       ml.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
-                       mr.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)))
+                       ml.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)) + .5)
+                       mr.run_forever(speed_sp = lim_speed(GAIN * (speed_y + speed_x)) - .5)
 
                 last_dx = dx
                 last_dy = dy
@@ -106,61 +134,62 @@ while not ts.value():
 ##Para determinar la distacia que recorre el robot se toma como parametro la posicion de los motorres##
                 pos =(((ml.position)+(mr.position))/2) ##Posicion absoluta de los dos motores
                 print ((ml.position),(mr.position)) ##Posicion absoluta de los dos motores
-                cm =(pos * 0.0275) ##Convercion a cm por grados
-                rue = (pos/360) ##convercion de grados por cm
-                print (pos,rue,cm)
+                print ((ml.speed_sp),(mr.speed_sp))
+#                s2.send_string(str(pos))
+#                cm=s2.recv()
+                cm = (pos * 0.0275,'cm')
+                print(cm)
                 sleep(1)
 
 ##Segunda condicion para la toma de decicion##
         else:
 
 ###Se detienen los motores grandes por estar en "run_forever"###
-#                ml.stop()
-#                mr.stop()
+                ml.stop()
+                mr.stop()
 ##Funcion de ori() para la orientacion inicial del muestreo##
 ##Funcion de muestra() para tomar muestra cada 45 grados del giro de la cabeza##
 ##Funcion de vuelta() para crear el giro del robot##
-#                ori()
-#                x = (ir.value(),1500,350,-350)
-#                sleep(1)
-#                print(x)
+                ori()
+                x = (ir.value(),1500,350,-350)
+                sleep(1)
 
-#                muestra()
-#                x1= (ir.value(),750,350,-350)
-#                sleep(1)
-#                print (x1)
+                muestra()
+                x1= (ir.value(),750,350,-350)
+                sleep(1)
 
-#                muestra()
+                muestra()
 
-#                muestra()
-#                y1 = (ir.value(),750,-350,350)
-#                sleep(1)
-#                print(y1)
+                muestra()
+                y1 = (ir.value(),750,-350,350)
+                sleep(1)
 
-#                muestra()
-#                y = (ir.value(),1500,-350,350)
-#                sleep(1)
-#                print(y)
+                muestra()
+                y = (ir.value(),1500,-350,350)
+                sleep(1)
 
-#                z = (x,x1,y1,y)
-#                z,a,b,c = max(z)
+                z = (x,x1,y1,y)
+                z,a,b,c = max(z)
 
-#                if z > 0:
+                #def angulo(a,b,c):
+                if b > c:
+                        an = a * 0.06
+                else:
+                        an = a * -0.06
 
-#                vuelta(a,b,c)
+#                s3.send_string(str(an))
+#                ani=s3.recv()
+                ##Sound.speak('Angle of rotation of' + ani).wait()
+
+                #angulo(a,b,c)
+                datos()
+                gdatos(cm,lum,an)
+
+                vuelta(a,b,c)
 ###Comando que formatea la posicion de los motores para volver a comensar un nuevo trayecto##
-#                ml.position = 0
-#                mr.position = 0
-#                ori()
-
-   ##             ml.run_timed(time_sp= a, speed_sp= b, stop_action='brake')
-     #          mr.run_timed(time_sp= a, speed_sp= c, stop_action='brake')
-      #         ml.wait_while('runnig')
-       #         mr.wait_while('runnig')
-                print("stop")
-                ml.stop()
-                mr.stop()
-                md.stop()
+                ml.position = 0
+                mr.position = 0
+                ori()
 
 ##Generacion de paros completos de los motores##
 ml.stop(stop_action="hold")
